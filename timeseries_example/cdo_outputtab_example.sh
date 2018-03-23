6f98f0a073f67f9f98c5e3418e7a238ce6d092da
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-03-23 08:26:21 +0100

    Add some maps.

---
+++ timeseries_example/cdo_outputtab_example.sh
@@ -0,0 +1,1 @@
+cdo outputtab,date,lon,lat,value -remapnn,lon=89.625_lat=25.0417 /scratch-shared/edwinhs/daily_discharge_aqueduct_flood_analyzer/m17/watch/historical_baseline/*annuaAvg*.nc > watch_BD_BRAHMAPUTRA_2651100_BAHADURABAD_table.txt
