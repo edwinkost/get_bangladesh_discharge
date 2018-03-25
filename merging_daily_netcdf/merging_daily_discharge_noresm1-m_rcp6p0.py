3417c147c5aede4923f99ce5c671418c1cfe0ee9
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2018-03-25 16:19:01 +0200

    merging_daily_discharge_noresm1-m_rcp6p0

---
+++ merging_daily_netcdf/merging_daily_discharge_noresm1-m_rcp6p0.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+clone_code = "M17"
+
+historical_folder_location = "/projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_noresm1-m/no_correction/non-natural/"
+
+#~ edwinhs@fcn1.bullx:/projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_noresm1-m/no_correction/non-natural$ ls -lah */M17/netcdf/discharge_dailyTot_output.nc 
+#~ -r-xr-xr-x 1 edwinsut aqueduct 936M Nov 16  2016    begin_from_1951/M17/netcdf/discharge_dailyTot_output.nc
+#~ -r-xr-xr-x 1 edwinsut aqueduct 3.0G Nov 14  2016 continue_from_1960/M17/netcdf/discharge_dailyTot_output.nc
+#~ -r-xr-xr-x 1 edwinsut aqueduct 1.6G Nov 16  2016 continue_from_1990/M17/netcdf/discharge_dailyTot_output.nc
+
+rcp_folder_location = "/projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2017_feb_rcp6p0/pcrglobwb_4_land_covers_edwin_parameter_set_noresm1-m/no_correction/rcp6p0/"
+
+#~ edwinhs@fcn1.bullx:/projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2017_feb_rcp6p0/pcrglobwb_4_land_covers_edwin_parameter_set_noresm1-m/no_correction/rcp6p0$ ls -lah */M17/netcdf/discharge_dailyTot_output.nc
+#~ -r-xr--r-- 1 edwinsut aqueduct 3.1G Feb 23  2017    begin_from_2006/M17/netcdf/discharge_dailyTot_output.nc
+#~ -r-xr--r-- 1 edwinsut aqueduct 3.0G Feb 24  2017 continue_from_2037/M17/netcdf/discharge_dailyTot_output.nc
+#~ -r-xr--r-- 1 edwinsut aqueduct 3.1G Apr 30  2017 continue_from_2067/M17/netcdf/discharge_dailyTot_output.nc
+#~ -r-xr--r-- 1 edwinsut aqueduct 201M Apr 30  2017 continue_from_2098/M17/netcdf/discharge_dailyTot_output.nc
+
+# names of netcdf files that will be merged
+netcdf_file_names = \
+[
+historical_folder_location +    "/begin_from_1951/" + clone_code + "/netcdf/discharge_dailyTot_output.nc", \
+historical_folder_location + "/continue_from_1960/" + clone_code + "/netcdf/discharge_dailyTot_output.nc", \
+historical_folder_location + "/continue_from_1990/" + clone_code + "/netcdf/discharge_dailyTot_output.nc", \
+rcp_folder_location        +    "/begin_from_2006/" + clone_code + "/netcdf/discharge_dailyTot_output.nc", \
+rcp_folder_location        + "/continue_from_2037/" + clone_code + "/netcdf/discharge_dailyTot_output.nc", \
+rcp_folder_location        + "/continue_from_2067/" + clone_code + "/netcdf/discharge_dailyTot_output.nc", \
+rcp_folder_location        + "/continue_from_2098/" + clone_code + "/netcdf/discharge_dailyTot_output.nc", \
+]
+
+# time period for each netcdf file
+start_years = [1951,\
+               1960,\
+               1984,\
+               2006,\
+               2030,\
+               2031,\
+               2033,\
+               2053,\
+               2084,\
+               ]
+end_years = []
+for i in range(0, len(start_years)-1):
+    end_years.append(start_years[i+1] - 1)
+end_years.append(2099)
+
+# output folder
+output_folder = "/scratch-shared/edwinhs/daily_discharge_aqueduct_flood_analyzer/m17/noresm1-m/rcp6p0/"
+# - make and go to the output folder
+#~ os.system('rm -r ' + output_folder + "/*")
+try:
+	os.makedirs(output_folder)
+except:
+	pass
+os.chdir(output_folder)
+
+# output netcdf file
+output_netcdf_file = "discharge_dailyTot_output_" + str(start_years[0]) + "-" + str(end_years[len(end_years)-1]) + ".nc4"
+
+# cdo command for merging
+cmd = 'cdo -L -f nc4 -z zip -mergetime '
+for i in range(0, len(start_years)):
+	cmd = cmd + '-selyear,' + str(start_years[i]) + "/" + str(end_years[i]) + " " + netcdf_file_names[i] + " "
+cmd = cmd + output_netcdf_file
+print(cmd)
+os.system(cmd)
+
+# Note that the total number of timesteps/days between 1951 and 2099 must be 54422. 
